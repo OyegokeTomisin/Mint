93ab6be13555a4761f81c748d19bf4ca3687bdb8
tomi <tomisin18@gmail.com>
2020-09-25 14:10:40 +0100

    Add commit history custom cell UI

--- Mint/CommitHistoryTableViewCell.swift
+++ Mint/CommitHistoryTableViewCell.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class CommitHistoryTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var commitAuthorLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 }
